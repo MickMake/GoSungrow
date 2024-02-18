cd3e683efeef567a9429a2ff72565aba5e574959
Mian Ali Ahmed <64776926+MAA-web@users.noreply.github.com>
2024-02-18 07:38:23 +0000

    the APP_KEY has been changed to ANDROIDE13EC118BD7892FE7AB5A3F20

--- iSolarCloud/const.go
+++ iSolarCloud/const.go
@@ -6,6 +6,7 @@ import "time"
 //goland:noinspection SpellCheckingInspection
 const (
 	DefaultHost      = "https://augateway.isolarcloud.com"
-	DefaultApiAppKey = "93D72E60331ABDCDC7B39ADC2D1F32B3"
+	//DefaultApiAppKey = "93D72E60331ABDCDC7B39ADC2D1F32B3"
+	DefaultApiAppKey = "ANDROIDE13EC118BD7892FE7AB5A3F20"
 	DefaultTimeout   = time.Second * 30
 )
